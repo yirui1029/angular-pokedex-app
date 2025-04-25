e7692034cefb397fa16a98ebc7c540774c3e6695
yirui Dupuy <yirui.hao@gmail.com>
2025-04-25 13:31:04 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, computed, signal } from '@angular/core';
+ import { POKEMON_LIST } from './pokemon-list.fake';
+ import { Pokemon } from './pokemon.model';
+import { PokemonBorderDirective } from './pokemon-border.directive';
+import { DatePipe } from '@angular/common';
+
+
+ 
+ @Component({
+   selector: 'app-root',
+   standalone: true,
+   imports: [PokemonBorderDirective,DatePipe],
+   
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css',
+ })
+ export class AppComponent {
+   pokemonList = signal(POKEMON_LIST);
+ 
+   size(pokemon: Pokemon) {
+     if (pokemon.life <= 15) {
+       return 'Petit';
+     }
+     if (pokemon.life >= 25) {
+       return 'Grand';
+     }
+ 
+     return 'Moyen';
+   }
+ 
+   incrementLife(pokemon: Pokemon) {
+     pokemon.life = pokemon.life + 1;
+   }
+ 
+   decrementLife(pokemon: Pokemon) {
+     pokemon.life = pokemon.life - 1;}}
